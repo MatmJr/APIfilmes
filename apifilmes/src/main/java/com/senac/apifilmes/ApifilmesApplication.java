00fa49e8baf8e3e8588a477a702797a1b2dcbabb
matmj <matmj@cin.ufpe.br>
2025-01-28 22:23:09 -0300

    teste

--- apifilmes/src/main/java/com/senac/apifilmes/ApifilmesApplication.java
+++ apifilmes/src/main/java/com/senac/apifilmes/ApifilmesApplication.java
@@ -2,6 +2,8 @@ package com.senac.apifilmes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApifilmesApplication {
@@ -9,5 +11,6 @@ public class ApifilmesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApifilmesApplication.class, args);
 	}
+
 
 }
